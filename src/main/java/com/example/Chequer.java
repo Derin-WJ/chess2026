f022dc112f78e47e2fa0f81ffeb5773ad188ad6c
Derin-WJ <derinsoysalis2009@gmail.com>
2026-04-16 15:04:58 +0000

    unbo

--- src/main/java/com/example/Chequer.java
+++ src/main/java/com/example/Chequer.java
@@ -19,6 +19,7 @@ public class Chequer extends Piece{
     }
     
     
+    
     // TO BE IMPLEMENTED!
     //return a list of every square that is "controlled" by this piece. A square is controlled
     //if the piece capture into it legally.
